bb9bb5cd1f1fb5e1360a70df9f9102bd02ae5fcb
Mintori09 <tvmthtr@gmail.com>
2026-04-16 12:18:42 +0700

    chore(scripts): add magika, jq and coreutils to scratchpad script PATH

--- modules/scripts/scratch.nix
+++ modules/scripts/scratch.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 let
   script = pkgs.writeShellScriptBin "temp" ''
+    export PATH="${
+      pkgs.lib.makeBinPath [
+        pkgs.bash
+        pkgs.magika
+        pkgs.jq
+        pkgs.coreutils
+      ]
+    }:$PATH"
     exec ${pkgs.bash}/bin/bash "${../../scripts/execute/scratch.sh}" "$@"
   '';
 in
