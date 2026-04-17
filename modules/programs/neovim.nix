1d9a8d27abd7799bf64e9cda0dc8e471c6ee38f3
Mintori09 <tvmthtr@gmail.com>
2026-04-17 18:19:19 +0700

    chore(flake): update description and refactor pkgs import style

---
+++ modules/programs/neovim.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    withNodeJs = true;
+    withPython3 = true;
+
+    extraPackages = with pkgs; [
+      gcc
+      gnumake
+      tree-sitter
+
+      # --- Go Development ---
+      go
+      gopls
+      gotools
+      golangci-lint
+      gomodifytags
+      impl
+
+      # --- JavaScript / TypeScript / Bun ---
+      bun
+      nodejs_22
+      typescript-language-server
+      vtsls
+      nodePackages.prettier
+      nodePackages.eslint
+
+      (python3.withPackages (ps: with ps; [
+        black
+        isort
+        pyflakes
+      ]))
+      pyright
+      ruff
+
+      # --- Lua  ---
+      lua-language-server
+      stylua
+
+      # --- CLI Utilities  ---
+      ripgrep
+      fd
+      fzf
+      lazygit
+      sqlite
+
+      pandoc
+      marksman
+    ];
+
+  };
+}
