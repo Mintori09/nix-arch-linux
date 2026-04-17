b0483d2d7c9eb0f095e421b9f4c4289ca0c42499
Mintori09 <tvmthtr@gmail.com>
2026-04-17 10:13:51 +0700

    refactor(yt-dlp): migrate from users.users to home-manager structure

--- modules/programs/yt-dlp.nix
+++ modules/programs/yt-dlp.nix
@@ -7,22 +7,34 @@ let
   configFile = "yt-dlp/config";
 in
 {
-  users.users.linuxmobile.packages = with pkgs; [
+  home.packages = with pkgs; [
     yt-dlp
     aria2
   ];
-  xdg.configFile."${configFile}".text = ''
-    -o ${config.home.homeDirectory}/Music/%(uploader)s/%(title)s.%(ext)s
-    --embed-thumbnail
-    --embed-metadata
-    -f bestaudio/best
-    --audio-format opus
-    --audio-quality 0
-    --continue
-    --no-overwrites
-    --restrict-filenames
-    --sponsorblock-remove sponsor,selfpromo,interaction
-    --downloader aria2c
-    --downloader-args aria2c:'--continue --min-split-size=5M --max-connection-per-server=4'
-  '';
+  xdg.configFile."${configFile}" = {
+    force = true;
+    text = ''
+      -o ${config.home.homeDirectory}/Desktop/Youtube/%(upload_date)s.%(title).100s.%(ext)s
+      --trim-filenames 100
+      --format "bv+ba/b"
+      --force-ipv4
+      --no-check-certificates
+      --embed-metadata
+      --embed-thumbnail
+      --embed-chapters
+      --write-auto-sub
+      --write-sub
+      --sub-langs en,vi
+      --embed-subs
+      --yes-playlist
+      --sponsorblock-remove sponsor,selfpromo,interaction
+      --downloader aria2c
+      --downloader-args aria2c:'--continue --min-split-size=5M --max-connection-per-server=4'
+      --cookies-from-browser zen-browser
+    '';
+  };
+
+  home.shellAliases = {
+    download-music = "yt-dlp -x --audio-format mp3 -o '${config.home.homeDirectory}/Desktop/Youtube/%(title)s.%(ext)s'";
+  };
 }
