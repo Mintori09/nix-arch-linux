cc938d7ee942f0ddcc0c873e250b75ec974d23c5
Mintori09 <tvmthtr@gmail.com>
2026-05-04 16:53:41 +0700

    chore: sync home-manager config

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -9,14 +9,12 @@
     ./fzf-rg-edit.nix
     ./fzf-preview.nix
     ./extract.nix
-    ./bash-generation.nix
     ./select-and-open-video.nix
     ./telepush.nix
     ./scratch.nix
     ./nano_usage.nix
     ./sleep-cycles.nix
     ./copy-files.nix
-    ./mdview.nix
     ./remove.nix
     ./compress-wrap.nix
   ];
