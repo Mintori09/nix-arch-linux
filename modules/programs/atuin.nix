3c36786ddcf2b73bef0ce60ba75baeb8d5a6f339
Mintori09 <tvmthtr@gmail.com>
2026-05-20 16:26:43 +0700

    fix: remove sops/catppuccin deps from atuin config
    Atuin no longer depends on sops-nix for key management or catppuccin
    for theming. The module was also missing from the module imports.

--- modules/programs/atuin.nix
+++ modules/programs/atuin.nix
@@ -5,9 +5,6 @@
   ...
 }:
 {
-  # Creates an infinite recursion if you do `catppuccin.atuin.enable = config.programs.atuin;`
-  catppuccin.atuin.enable = true;
-
   programs = {
     atuin = {
       enable = true;
@@ -17,18 +14,11 @@
       flags = [ "--disable-up-arrow" ];
       settings = {
         auto_sync = true;
-        key_path = config.sops.secrets.atuin_key.path;
         sync_frequency = "5m";
         update_check = false;
         sync.records = true;
         dotfiles.enabled = false;
       };
-    };
-  };
-
-  sops = {
-    secrets = {
-      atuin_key.path = "${config.xdg.dataHome}/atuin/key";
     };
   };
 }
--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -23,6 +23,7 @@
     ./pinta.nix
     ./alacritty.nix
     ./qutebrowser.nix
+    ./atuin.nix
     ./packages.nix
     ./zed
     ./yt-dlp.nix
