f4abb91a2e24c70a6358006461ca4fd241a70c5f
Mintori09 <tvmthtr@gmail.com>
2026-05-20 16:26:29 +0700

    feat: add catppuccin and sops-nix flake inputs
    Add catppuccin theming module and sops-nix secret management as flake
    inputs, following nixpkgs for dependency consistency.

--- flake.nix
+++ flake.nix
@@ -15,6 +15,16 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -23,6 +33,8 @@
       home-manager,
       nixgl,
       spicetify-nix,
+      catppuccin,
+      sops-nix,
       ...
     }:
     let
@@ -41,6 +53,8 @@
         modules = [
           ./home.nix
           spicetify-nix.homeManagerModules.spicetify
+          catppuccin.homeModules.catppuccin
+          sops-nix.homeManagerModules.sops
         ];
 
         extraSpecialArgs = {
