ce08329ec70f9fe05d1b942d0078140ed567d17b
Mintori09 <tvmthtr@gmail.com>
2026-05-22 02:07:14 +0700

    feat(kitty): switch to Tokyo Night color scheme
    Replace custom color config with Tokyo Night theme palette. Adds background_opacity=0.9, configures tab bar fade style, and sets cursor shape to block.

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -22,58 +22,110 @@ in
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
-
-      shell = "zsh";
-
-      cursor = "#b25424";
-      cursor_text_color = "#24242e";
-      cursor_shape = "block";
-
       hide_window_decorations = "yes";
       confirm_os_window_close = 0;
       dynamic_background_opacity = "yes";
-      active_border_color = "#515167";
-      inactive_border_color = "#24242e";
-      bell_border_color = "#5151e6";
-      wayland_titlebar_color = "#333342";
-      macos_titlebar_color = "#333342";
+      shell = "zsh";
+      background_opacity = "0.9";
 
-      url_color = "#cecee3";
+      foreground = "#a9b1d6";
+      background = "#1a1b26";
 
-      active_tab_foreground = "#fbf9f9";
-      active_tab_background = "#24242e";
-      inactive_tab_foreground = "#b1a9a5";
-      inactive_tab_background = "#333342";
-      tab_bar_background = "#333342";
+      color0 = "#414868";
+      color8 = "#414868";
 
-      foreground = "#a1a1b5";
-      background = "#161e2e";
-      selection_foreground = "#a1a1b5";
-      selection_background = "#333342";
+      color1 = "#f7768e";
+      color9 = "#f7768e";
 
-      mark1_foreground = "#24242e";
-      mark1_background = "#6363ee";
-      mark2_foreground = "#24242e";
-      mark2_background = "#8e8580";
-      mark3_foreground = "#24242e";
-      mark3_background = "#cb5c25";
+      color2 = "#73daca";
+      color10 = "#73daca";
 
-      color0 = "#24242e";
-      color1 = "#7676f4";
-      color2 = "#ec7336";
-      color3 = "#fe8c52";
-      color4 = "#767693";
-      color5 = "#ec7336";
-      color6 = "#8a8aad";
-      color7 = "#a1a1b5";
-      color8 = "#5b5b76";
-      color9 = "#f37b3f";
-      color10 = "#333342";
-      color11 = "#515167";
-      color12 = "#737391";
-      color13 = "#cecee3";
-      color14 = "#e66e33";
-      color15 = "#ebebff";
+      color3 = "#e0af68";
+      color11 = "#e0af68";
+
+      color4 = "#7aa2f7";
+      color12 = "#7aa2f7";
+
+      color5 = "#bb9af7";
+      color13 = "#bb9af7";
+
+      color6 = "#7dcfff";
+      color14 = "#7dcfff";
+
+      color7 = "#c0caf5";
+      color15 = "#c0caf5";
+
+      cursor = "#c0caf5";
+      cursor_text_color = "#1a1b26";
+
+      selection_foreground = "none";
+      selection_background = "#28344a";
+
+      url_color = "#9ece6a";
+
+      active_border_color = "#3d59a1";
+      inactive_border_color = "#101014";
+      bell_border_color = "#e0af68";
+
+      tab_bar_style = "fade";
+      tab_fade = 1;
+      active_tab_foreground = "#3d59a1";
+      active_tab_background = "#16161e";
+      active_tab_font_style = "bold";
+      inactive_tab_foreground = "#787c99";
+      inactive_tab_background = "#16161e";
+      inactive_tab_font_style = "bold";
+      tab_bar_background = "#101014";
+
+      macos_titlebar_color = "#16161e";
+
+      # cursor = "#b25424";
+      # cursor_text_color = "#24242e";
+      # cursor_shape = "block";
+      #
+      #
+      # active_border_color = "#515167";
+      # inactive_border_color = "#24242e";
+      # bell_border_color = "#5151e6";
+      # wayland_titlebar_color = "#333342";
+      # macos_titlebar_color = "#333342";
+      #
+      # url_color = "#cecee3";
+      #
+      # active_tab_foreground = "#fbf9f9";
+      # active_tab_background = "#24242e";
+      # inactive_tab_foreground = "#b1a9a5";
+      # inactive_tab_background = "#333342";
+      # tab_bar_background = "#333342";
+      #
+      # foreground = "#a1a1b5";
+      # background = "#161e2e";
+      # selection_foreground = "#a1a1b5";
+      # selection_background = "#333342";
+      #
+      # mark1_foreground = "#24242e";
+      # mark1_background = "#6363ee";
+      # mark2_foreground = "#24242e";
+      # mark2_background = "#8e8580";
+      # mark3_foreground = "#24242e";
+      # mark3_background = "#cb5c25";
+      #
+      # color0 = "#24242e";
+      # color1 = "#7676f4";
+      # color2 = "#ec7336";
+      # color3 = "#fe8c52";
+      # color4 = "#767693";
+      # color5 = "#ec7336";
+      # color6 = "#8a8aad";
+      # color7 = "#a1a1b5";
+      # color8 = "#5b5b76";
+      # color9 = "#f37b3f";
+      # color10 = "#333342";
+      # color11 = "#515167";
+      # color12 = "#737391";
+      # color13 = "#cecee3";
+      # color14 = "#e66e33";
+      # color15 = "#ebebff";
     };
 
     keybindings = {
