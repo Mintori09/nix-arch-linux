1f27dbc0bf690b598771e04e0634bdc6723b8f8a
Mintori09 <tvmthtr@gmail.com>
2026-06-01 08:10:59 +0700

    feat(rclone-sync): add zsh tab completion

--- modules/scripts/rclone-sync.nix
+++ modules/scripts/rclone-sync.nix
@@ -1,11 +1,37 @@
 { pkgs, ... }:
 let
   helpers = import ./_helpers.nix { inherit pkgs; };
+
+  rcloneSyncCompletion = pkgs.writeTextFile {
+    name = "rclone-sync-zsh-completion";
+    destination = "/share/zsh/site-functions/_rclone-sync";
+    text = ''
+      #compdef rclone-sync
+
+      _rclone-sync() {
+        _arguments \
+          '--config[tar & upload ~/.config to Drive]' \
+          '--zen[tar & upload Zen browser profiles to Drive]' \
+          '--obsidian[rclone sync Obsidian vault to Drive]' \
+          '--all[sync config + zen + obsidian]' \
+          '(-n)--dry-run[preview without syncing]' \
+          '(-n --dry-run)-n[preview without syncing]' \
+          '(-h)--help[show help message]' \
+          '(-h --help)-h[show help message]'
+      }
+
+      _rclone-sync "$@"
+    '';
+  };
 in
 {
-  home.packages = helpers.mkScriptPackage {
-    name = "rclone-sync";
-    entry = "${../../scripts/execute/rclone-sync.ts}";
-    extraPathPackages = [ pkgs.rclone ];
-  };
+  home.packages =
+    helpers.mkScriptPackage {
+      name = "rclone-sync";
+      entry = "${../../scripts/execute/rclone-sync.ts}";
+      extraPathPackages = [ pkgs.rclone ];
+    }
+    ++ [
+      rcloneSyncCompletion
+    ];
 }
