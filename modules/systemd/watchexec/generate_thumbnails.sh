8ce6a202ae7c58bfea6aa43dc4b6b88ff818e33c
Mintori09 <tvmthtr@gmail.com>
2026-07-04 13:56:47 +0700

    feat(watchexec): add video duration column to thumbnail list
    - Use ffprobe to extract video durations in sexagesimal format
    
    - Format the duration string to drop leading hours if zero
    
    - Render the duration in a new markdown table column

--- modules/systemd/watchexec/generate_thumbnails.sh
+++ modules/systemd/watchexec/generate_thumbnails.sh
@@ -39,8 +39,8 @@ cssclasses:
 tags:
   - moc
 ---
-| Video Name | Thumbnail | Absolute Path |
-| :--- | :---: | :--- |
+| Video Name | Duration | Thumbnail | Absolute Path |
+| :--- | :---: | :---: | :--- |
 EOF
 
 shopt -s nocaseglob
@@ -52,6 +52,18 @@ for video in *.{mp4,mkv,avi,mov,flv,wmv}; do
 	thumb_path_md="thumbnails/${filename_no_ext}.jpg"
 	abs_path=$(realpath "$video")
 
+	duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "$video" 2>/dev/null | cut -d'.' -f1)
+	if [ -z "$duration" ]; then
+		duration="N/A"
+	else
+		if [[ $duration =~ ^([0-9]):([0-9]{2}):([0-9]{2})$ ]]; then
+			duration="0${BASH_REMATCH[1]}:${BASH_REMATCH[2]}:${BASH_REMATCH[3]}"
+		fi
+		if [[ $duration == 00:* ]]; then
+			duration="${duration#00:}"
+		fi
+	fi
+
 	if [ -f "$thumb_path_real" ]; then
 		echo "Using cached thumbnail for: $video"
 	else
@@ -61,7 +73,7 @@ for video in *.{mp4,mkv,avi,mov,flv,wmv}; do
 			ffmpeg -y -nostdin -i "$video" -vframes 1 -vf "scale=160:-1" -v quiet "$thumb_path_real"
 		fi
 	fi
-	echo "| $filename | <img src=\"$thumb_path_md\" width=\"120\"> | [Open Video](<file://$abs_path>) |" >>"$OUTPUT_FILE"
+	echo "| $filename | $duration | <img src=\"$thumb_path_md\" width=\"120\"> | [Open Video](<file://$abs_path>) |" >>"$OUTPUT_FILE"
 done
 shopt -u nocaseglob
 
